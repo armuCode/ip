a61e4d6473097c743d204633f9c5d4e51149dbc0
armucode <armucode@gmail.com>
2024-04-08 17:55:21 -0500

    Se agrega el endpoint que se consume su propio endpoint  tan pronto se carga el home

--- utils/useUserIp.tsx
+++ utils/useUserIp.tsx
@@ -1,11 +1,25 @@
-import { useEffect, useState } from "react";
-import { get } from "browser-cookies";
-export const useUserIp = () => {
-  const [ip, setUserIp] = useState<string>("");
+// hooks/useIP.js
+import { useState, useEffect } from "react";
+
+const useUserIP = () => {
+  const [ip, setIP] = useState("");
+
   useEffect(() => {
-    const userIp = get("user-ip") ?? "Unknow";
-    setUserIp(userIp);
+    const fetchIP = async () => {
+      try {
+        const response = await fetch("/api/hello"); // Ruta a tu endpoint de API
+        const data = await response.json();
+        setIP(data.ip);
+      } catch (error) {
+        console.error("Error fetching IP:", error);
+        setIP("Unknown");
+      }
+    };
+
+    fetchIP();
   }, []);
 
   return ip;
 };
+
+export default useUserIP;
